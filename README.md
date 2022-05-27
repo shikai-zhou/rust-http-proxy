0b296ee21ff44b6b8c149204a070cbd2e2dfd45d
Shikai <shikai55@gmail.com>
2022-05-27 17:07:05 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# About
+
+Simple http web proxy in rust. Fast and multithreaded using tokio runtime.
+
+# Dependencies
+
+- tokio
+
+# Working
+
+- Basic proxy connections
+
+# Future Features
+
+- Caching
+- Blacklisting
+- Telemetry
---
+++ src/main.rs
@@ -0,0 +1,88 @@
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::AsyncWriteExt;
+use tokio::io;
+use std::env;
+use std::str;
+
+async fn process_socket(mut inbound: TcpStream) -> io::Result<()>{
+    // do work with socket here
+    inbound.readable().await?;
+    let mut buf = [0; 256];
+    inbound.try_read(&mut buf).unwrap();
+    let string = str::from_utf8(& buf).unwrap();
+    // println!("{}", string);
+    let v: Vec<&str> = string.split(' ').collect();
+    // println!("{}", v[1]);
+    let mut outbound;
+    if v[0] == "CONNECT" {
+        let uri = parse_uri(&v).await?;
+        outbound = TcpStream::connect(uri).await?;
+        let mut payload = v[2].to_string();
+        payload.push_str(&"200 Connection established\r\n\r\n".to_string());
+        inbound.try_write(payload.as_bytes()).unwrap();
+    } else {
+        //catch all for other requests like GET POST etc
+        let uri = parse_uri(&v).await?;
+        outbound = TcpStream::connect(uri).await?;
+        outbound.try_write(&buf).unwrap();
+    }
+    let (mut ri, mut wi) = inbound.split();
+    let (mut ro, mut wo) = outbound.split();
+
+    let client_to_server = async {
+        io::copy(&mut ri, &mut wo).await?;
+        wo.shutdown().await
+    };
+
+    let server_to_client = async {
+        io::copy(&mut ro, &mut wi).await?;
+        wi.shutdown().await
+    };
+
+    tokio::try_join!(client_to_server, server_to_client)?;
+
+    Ok(())
+}
+
+async fn parse_uri(v : &Vec<&str>) -> io::Result<String>{
+    //get uri from request
+    //TODO: Check that request is valid
+    // let uri = v[1];
+    // let pos = uri.find("://").unwrap_or_else(|| usize::MAX);
+    // let len = uri.len();
+    // let hostname : str;
+    // if pos == usize::MAX {
+    //     let hostname = &uri;
+    // } else {
+    //     let hostname = &&uri[pos+3..len];
+    // }
+    let hostname = v[1];
+    
+    Ok(hostname.to_string())
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let mut server_addr = "127.0.0.1".to_string();
+    if args.len() < 2 {
+        server_addr.push_str(":8080");
+    } else {
+        server_addr.push_str(":");
+        server_addr.push_str(&args[1]);
+    }
+    let listener = TcpListener::bind(server_addr).await?;
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+        tokio::spawn( async move {
+            
+            match process_socket(socket).await {
+                Err(e) => println!("Connection Error! {}", e),
+                _ => ()
+            };
+        });
+        
+    }
+
+}
